d7820322a71f6c367c4a01bef25506a8d7121eb9
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-02-22 17:21:12 +0600

    fix: navbar-footer is reviewed

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,20 +1,15 @@
 import styles from "../../styles/Home.module.css";
+import Image from 'next/image';
 
 const Footer = () => {
   return (  
     <>
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+          Powered by
+          <Image src="/CB logo.png" alt="Vercel Logo" width={158} height={64} className={styles.logo} />
       </footer>
     </>
   );
-}
+};
  
 export default Footer;
