342157cf80f3c787845bd89b28631760ebf48ebe
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-03-07 13:03:50 +0600

    add: banner css module

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -1,30 +1,27 @@
-import { Image } from "react-bootstrap";
+import styles from "./Banner.module.css";
+import classNames from "classnames";
+import { BsListNested } from "react-icons/bs";
 
 const Banner = ({ source }) => {
   return (
     <>
       <div
+        className={styles.banner}
         style={{
-          width: "100%",
           backgroundImage: `url(${source})`,
-          position: "relative",
-          height: "500px",
-          backgroundSize: "cover",
         }}
       >
-        <div className="banner">
-          <img
-            src="/logo/cb-logo.png"
-            alt=""
-            style={{ width: "300px", marginBottom: "10px" }}
-          />
+      
+        <div className={styles.bannerTitle}>
+          <img src="/logo/cb-logo.png" alt="" className={styles.logo} />
           <h5 className="mb-4 pl-4">
             Explore our schools to find your perfect program
           </h5>
-          <a href="#" className="custom-btn ml-4">
+          <a href="#" className={classNames(styles.customBtn, "ml-4")}>
             All Courses
           </a>
         </div>
+
       </div>
     </>
   );
