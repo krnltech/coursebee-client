dbe026d4d0e39c3c553428aa08c8293ea756bbc1
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-03-14 12:49:13 +0600

    add: bangla lang and modified search bar on banner

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,4 +1,6 @@
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { BsFillGrid3X3GapFill } from 'react-icons/bs';
+
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,7 +10,7 @@ const NavbarComps = () => {
   return (
     <>
       <Navbar bg="light" expand="lg">
-        <div className="container">
+        <div className="container-fluid">
           <Link href="/">
             <a>
               <Image
@@ -21,21 +23,25 @@ const NavbarComps = () => {
           </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <BsFillGrid3X3GapFill />
+              <Nav.Link href="#catagories">ক্যাটাগরি</Nav.Link>
+            </Nav>
             <Form inline>
               <FormControl
                 type="text"
                 placeholder="Search"
                 className="mr-sm-2"
               />
-              <Button variant="outline-success">Search</Button>
+              <Button variant="outline-success">Search</Button>              
             </Form>
 
             <div className={styles.content}>
               <Nav className="mr-auto">
-                <Nav.Link href="#courses">Courses</Nav.Link>
-                <Nav.Link href="#training">Training</Nav.Link>
-                <Nav.Link href="#mentor">Become a mentor</Nav.Link>
-                <Nav.Link href="#login">Login</Nav.Link>
+                <Nav.Link href="#courses">কোর্স</Nav.Link>
+                <Nav.Link href="#training">ট্রেইনিং</Nav.Link>
+                <Nav.Link href="#mentor">মেন্টর হন</Nav.Link>
+                <Nav.Link href="#login">লগইন</Nav.Link>
               </Nav>
             </div>
           </Navbar.Collapse>
