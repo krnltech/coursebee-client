b338ddf2fe7afce6b7b4e146bf164fc1c9ee7326
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-03-15 15:47:18 +0600

    add: course banner is finished

--- components/course-banner/CourseBanner.js
+++ components/course-banner/CourseBanner.js
@@ -1,15 +1,41 @@
 import classNames from "classnames";
 import styles from "./CourseBanner.module.css";
 
-const CiurseBanner = () => {
+import { AiFillStar, AiOutlineHeart, AiOutlineShareAlt } from "react-icons/ai";
+import { Button } from "react-bootstrap";
+
+
+const CourseBanner = ({ title, secondTitle, rating, numberOfRatings, numberOfStudents, instructorName }) => {
+
+  // Course Rating 
+  const renderRating = (ratingNumber) => {
+    const stars = [];
+    for (var i = 0; i < ratingNumber; i++) {
+      stars.push(<AiFillStar className={styles.ratingIcon} />);
+    }
+    return stars.map((star, key) => {
+      return <>{star}</>;
+    });
+  };
+
   return (
     <>
       <div className={classNames("container-fluid", styles.courseBanner)}>
-        <h2></h2>
-        <h4></h4>        
+        <h2>{title}</h2>
+        <h4>{secondTitle}</h4>
+        <span className={classNames(styles.rating, "mt-2")}>
+
+          {renderRating(rating)}
+          <span className={styles.ratingNumber}>({numberOfRatings} ratings)  {numberOfStudents} students</span>
+        </span>
+        Created by <a href="#">{instructorName}</a>
+        <br />
+        <br />
+        <Button variant="outline-success" style={{ marginRight:"10px" }}><AiOutlineHeart/> WishList</Button>
+        <Button variant="outline-warning"><AiOutlineShareAlt/> Share</Button>
       </div>
     </>
   );
 };
 
-export default CiurseBanner;
+export default CourseBanner;
--- pages/courseDetails.js
+++ pages/courseDetails.js
@@ -1,24 +1,42 @@
-import styles from "../styles/Home.module.css";
+import styles from "../styles/CourseDetails.module.css";
 import classNames from "classnames";
-import { Button } from "react-bootstrap";
 
 import HeadComps from "../components/Head";
 import CourseBanner from "../components/course-banner/CourseBanner";
 
 const CourseDetails = () => {
-  return (  
+  return (
     <>
       {/* HeadComps */}
       <section className="layer">
-        <HeadComps title="Coursebee | Details" iconSource="/icon/coursebee.ico" />
+        <HeadComps
+          title="Coursebee | Details"
+          iconSource="/icon/coursebee.ico"
+        />
       </section>
 
       {/* Main Banner of Course Details */}
       <section className={styles.layer}>
-        < CourseBanner />
+        <CourseBanner
+          title="Complete Python Bootcamp"
+          secondTitle="Learn Python like a Professional Start from the basics and go all the way to creating your own applications and games"
+          rating={4}
+          numberOfRatings="300"
+          numberOfStudents="700"
+          instructorName="Zahid Hossain"
+        />
+      </section>
+
+      <section className={classNames(styles.layer, "container")}>
+        <div className="row">
+          <div className="col-md-8" style={{ border: "1px solid red", padding: "5px" }}>
+          </div>
+          <div className="col-md-4" style={{ border: "1px solid red", padding: "5px" }}>
+          </div>
+        </div>
       </section>
     </>
   );
-}
- 
+};
+
 export default CourseDetails;
