dc8058555f0970af7cf543ffb67988974d2d3c40
zahid0033 <zahidhossain0033@gmail.com>
2021-03-20 16:03:08 +0600

    cart added

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import Navbar from "./navbar/Navbar";
+import Cart from "./cart/Cart";
 import Footer from "./footer/Footer";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <Cart/>
       {children}
       <Footer />
     </>
