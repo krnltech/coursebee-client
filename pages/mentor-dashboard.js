8697bc737500619a0a0aa971c4a7a4c0f43544bb
zahid0033 <zahidhossain0033@gmail.com>
2021-03-24 16:28:33 +0600

    auth added

--- pages/mentor-dashboard.js
+++ pages/mentor-dashboard.js
@@ -1,9 +1,13 @@
 import Sidebar from "../components/sidebar/Sidebar";
+import { useSelector,useDispatch } from "react-redux";
 
 const MentorDashboard = () => {
+    const { user,isAuthenticated } = useSelector((state) => state.auth);
+    console.log(user,isAuthenticated)
   return (
     <>
       <Sidebar />
+      <h1>Successful</h1>
     </>
   );
 };
