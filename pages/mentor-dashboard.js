079d67a3db3ae46e1ccfc266b3ec112cabd1edaf
zahid0033 <zahidhossain0033@gmail.com>
2021-03-24 18:16:51 +0600

    dashboard test

--- pages/mentor-dashboard.js
+++ pages/mentor-dashboard.js
@@ -3,11 +3,14 @@ import { useSelector,useDispatch } from "react-redux";
 
 const MentorDashboard = () => {
     const { user,isAuthenticated } = useSelector((state) => state.auth);
-    console.log(user,isAuthenticated)
+    console.log(user.user.email,isAuthenticated)
   return (
     <>
       <Sidebar />
       <h1>Successful</h1>
+        <h5>{user.user.email}</h5>
+        <h5>{user.user.type}</h5>
+        <h5>{user.user.fullname}</h5>
     </>
   );
 };
