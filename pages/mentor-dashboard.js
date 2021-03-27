8b8d28dfd0f650cd154196e5349e5bf1c578b74d
zahid0033 <zahidhossain0033@gmail.com>
2021-03-27 16:18:15 +0600

    edit

--- pages/mentor-dashboard.js
+++ pages/mentor-dashboard.js
@@ -2,15 +2,15 @@ import Sidebar from "../components/sidebar/Sidebar";
 import { useSelector,useDispatch } from "react-redux";
 
 const MentorDashboard = () => {
-    const { user,isAuthenticated } = useSelector((state) => state.auth);
+    const { user,isAuthenticated } = useSelector((state) => state.auth.user);
     console.log(user.user.email,isAuthenticated)
   return (
     <>
       <Sidebar />
       <h1>Successful</h1>
-        <h5>{user.user.email}</h5>
-        <h5>{user.user.type}</h5>
-        <h5>{user.user.fullname}</h5>
+        <h5>{user.email}</h5>
+        <h5>{user.type}</h5>
+        <h5>{user.fullname}</h5>
     </>
   );
 };
